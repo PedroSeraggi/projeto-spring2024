62aa089eb9dfd3859162cfb1e80c9695811ab522
Pedro Seraggi <brener.reis@fatec.sp.gov.br>
2024-10-05 12:08:44 -0300

    first commit

---
+++ src/test/java/br/gov/sp/fatec/projeto_spring2024/ProjetoSpring2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gov.sp.fatec.projeto_spring2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoSpring2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
